0125e712329275498612322f02ebb8343f3dde71
Senin Ashraf <seninashraf765@gmail.com>
2026-09-25 22:20:26 +0530

    authentication managment

--- test_auth.py
+++ test_auth.py
@@ -1,12 +1,17 @@
+import bcrypt
 from dao.authenticationdao import AuthDao
 
 auth_dao = AuthDao()
 
-admin = auth_dao.find_admin("senin")
+x=input('Enter Username:')
+admin = auth_dao.find_admin(x)
 
 if admin is None:
     print("Admin not found")
 else:
-    print("Admin found")
-    print("Username:", admin.username)
-    print("Password hash:", admin.passwordHash)
+   entered_password=input('Enter password:')
+   if bcrypt.checkpw(entered_password.encode("utf-8"),
+        admin.passwordHash.encode("utf-8")):
+        print("Login successful")
+   else:
+        print("Wrong password")
